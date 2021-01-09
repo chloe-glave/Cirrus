509b2658e4cc56ffe297ffa9c6a6d25f1576685a
Chloe (Desktop) <cragzu@gmail.com>
2021-01-09 15:14:25 -0800

    add commas

--- bot.py
+++ bot.py
@@ -16,19 +16,21 @@ AWS_SECRET_KEY = os.getenv('SECRET_KEY')
 AWS_PROFILE_NAME = os.getenv('PROFILE_NAME')
 
 session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY
-    aws_secret_access_key=AWS_SECRET_KEY
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
     profile_name=AWS_PROFILE_NAME
 )
 
 db_client = session.resource('dynamodb')
 bot = commands.Bot(command_prefix="!")
 
+
 # respond to ping message
 @bot.command(name='ping', help='Responds to ping for testing purposes')
 async def ping(ctx):
     response = f'Pong! 🏓 Response time: {round(bot.latency, 3)}ms'
     await ctx.send(response)
+
 
 # add assignment
 @bot.command(name="add", help="adds an assignment to the database")
