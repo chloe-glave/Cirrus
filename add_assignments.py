8bc21892d89713096d5eef2c846a73c1bf9d79ce
jkcadee <54956049+jkcadee@users.noreply.github.com>
2021-01-09 14:46:16 -0800

    aws additions

--- add_assignments.py
+++ add_assignments.py
@@ -9,13 +9,24 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-db_client = boto3.resource('dynamodb')
+# AWS Credentials
+AWS_ACCESS_KEY = os.getenv('ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('SECRET_KEY')
+AWS_PROFILE_NAME = os.getenv('PROFILE_NAME')
+
+session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY
+    aws_secret_access_key=AWS_SECRET_KEY
+    profile_name=AWS_PROFILE_NAME
+)
+
+db_client = session.resource('dynamodb')
 bot = commands.Bot(command_prefix="!")
 
 @bot.command(name="add")
-async def add_assignment_command(ctx):
+async def add_assignment_command(ctx, assignment_name, help="adds an assignment to the database"):
     info = message.content.split()
-    body = {"id": random.randint(), "assignment_name": info[1]}
+    body = {"id": random.randint(), "assignment_name": assignment_name}
 
     table = db_client.Table("CirrusBotMessages")
 
