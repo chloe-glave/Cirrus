c5256d9307ee22a8a607de9d97e47c25bbbb570f
Chloe (Desktop) <cragzu@gmail.com>
2021-01-09 14:28:53 -0800

    change client to bot class for better functionality

--- bot.py
+++ bot.py
@@ -1,36 +1,19 @@
 import os
-import random
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+bot = commands.Bot(command_prefix='!')
 
 
 # respond to ping message
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command(name='ping')
+async def ping(ctx):
+    response = 'pong!'
+    await ctx.send(response)
 
-    if message.content == 'ping':
-        response = 'pong!'
-        await message.channel.send(response)
-
-client.run(TOKEN)
+bot.run(TOKEN)
