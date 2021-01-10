adc5ae4f9e80949dbc0a28a3716ea1ef60dd2443
jkcadee <54956049+jkcadee@users.noreply.github.com>
2021-01-09 21:00:12 -0800

    Removed profile name and print statements

--- bot.py
+++ bot.py
@@ -16,21 +16,14 @@ bot = commands.Bot(command_prefix='!')
 
 # AWS Credentials
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
-print(AWS_ACCESS_KEY)
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-print(AWS_SECRET_KEY)
-AWS_PROFILE_NAME = os.getenv('PROFILE_NAME')
-print(AWS_PROFILE_NAME)
 AWS_REGION_NAME = os.getenv('REGION_NAME')
-print(AWS_REGION_NAME)
 
 session = boto3.Session(
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_KEY,
     region_name=AWS_REGION_NAME
 )
-
-print(session)
 
 db_client = session.resource('dynamodb')
 bot = commands.Bot(command_prefix="!")
